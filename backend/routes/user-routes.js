167c49493839434e3617ed698865d21882033c6f
Sarvesh <>
2026-05-07 16:30:15 +0530

    OTP-Mail Logout

--- backend/routes/user-routes.js
+++ backend/routes/user-routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import * as userControllers from "../controllers/user-controller.js";
+
 const router = Router();
 
 router.post("/register", userControllers.registerUser);
 router.post("/verify", userControllers.verification);
 router.post("/login", userControllers.loginUser);
+router.post("/forgot-password", userControllers.forgotPassword);
 
 export default router;
