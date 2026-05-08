13f75666f81e5d33a3b6a1551c5c367c85445397
Sarvesh <>
2026-05-08 16:22:09 +0530

    USER BACKEND FINISHED

--- backend/routes/user-routes.js
+++ backend/routes/user-routes.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import * as userControllers from "../controllers/user-controller.js";
+import { isAuthenticated } from "../middleware/auth-middleware.js";
 
 const router = Router();
 
@@ -7,5 +8,9 @@ router.post("/register", userControllers.registerUser);
 router.post("/verify", userControllers.verification);
 router.post("/login", userControllers.loginUser);
 router.post("/forgot-password", userControllers.forgotPassword);
+router.post("/verify-otp/:email", userControllers.verifyOtp);
+router.post("/confirm-password/:email", userControllers.confirmPassword);
+
+router.post("/logout", isAuthenticated, userControllers.logout);
 
 export default router;
